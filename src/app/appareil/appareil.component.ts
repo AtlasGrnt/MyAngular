dd201bb5ca43f20c72c7bb5af6a9b74623c11aa9
Grégory Grenot <gregory.grenot@gmail.com>
2020-01-10 15:01:45 +0100

    FEAT: Removing forms, adding button to switch on/off each devices

--- src/app/appareil/appareil.component.ts
+++ src/app/appareil/appareil.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { AppareilService } from '../services/appareil.service';
 
 @Component({
   selector: 'app-appareil',
@@ -10,8 +11,9 @@ export class AppareilComponent implements OnInit {
   @Input() appareilName: string;
   @Input() appareilStatus: string;
   @Input() appareilPosition: string;
+  @Input() indexOfAppareil:number;
 
-  constructor() { }
+  constructor(private appareilService: AppareilService) { }
 
   ngOnInit() {
   }
@@ -24,6 +26,13 @@ export class AppareilComponent implements OnInit {
     } else if(this.appareilStatus === 'éteint'){
       return 'red';
     }
+  }
+
+  onSwitchOn(){
+    this.appareilService.switchOnOne(this.indexOfAppareil);
+  }
+  onSwitchOff(){
+    this.appareilService.switchOffOne(this.indexOfAppareil);
   }
 
 }
